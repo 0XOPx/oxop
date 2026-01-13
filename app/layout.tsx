c4eb0ab47793242e3d0537df028fc35258120577
OXOP <minbartek41@gmail.com>
2026-01-13 17:42:26 +0100

    l

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "OXOP BMP Official's site.",
+    title: "OXOP site.",
     description: "Programmer from Poland | Black MIDI & coding projects"
 };
 
